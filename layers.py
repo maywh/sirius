718a8c09bae7a890cf410b855b2e80baba22fd33
maywh <jmhharmer@hotmail.co.uk>
2019-04-16 13:45:20 +0100

    updated layers, removed obselete object inheritence

--- layers.py
+++ layers.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-class Layer(object):
+class Layer:
     # Parent layer
     def __init__(self, n_target, n_neurons, weight_initialisation):
-        self.weights = self.initialise_weights(n_target, n_neurons, weight_initialisation)
-        self.bias = self.inilitalise_bias(n_neurons)
+        self.weights = self._initialise_weights(n_target, n_neurons, weight_initialisation)
+        self.bias = self._inilitalise_bias(n_neurons)
 
     def _initialise_weights(self, n_target, n_neurons, weight_initialisation):
         if weight_initialisation == 'uniform':
@@ -16,29 +16,43 @@ class Layer(object):
         bias = np.zeros(n_neurons)
         return bias
 
+    def _sigmoid_activation(self, product):
+        return 1 / (1 + np.exp(-1 * product))
 
-class HiddenLayer(Layer):
+    def _relu_activation(self, product):
+        return product
+
+    def _softmax_activation(self, product):
+        return np.exp(product)/np.sum(np.exp(product))
+
+
+class HiddenLayer:
     # NN hidden layer
     def __init__(self, activation, n_input, n_neurons, weight_initialisation='uniform'):
         super().__init__(n_input, n_neurons, weight_initialisation)
         self._activation = activation
 
-    def _activation_function(self, activation, product):
-        if activation == "sigmoid":
-            activated = 1 / (1 + np.exp(-1 * product))
-            return activated
+    def _activate(self, product):
+        if self._activation == "sigmoid":
+            z =  self._sigmoid_activation(product)
+        elif self.activation == "relu":
+            z =  self._relu_activation(product)
+        else:
+            pass
+        return z
 
 
-class OutputLayer(Layer):
+class OutputLayer:
     # NN output layer
-    def __init__(self, n_output, n_neurons, weight_initialisation='uniform'):
+    def __init__(self, activation, n_output, n_neurons, weight_initialisation='uniform'):
         super().__init__(n_output, n_neurons, weight_initialisation)
         self.n_output = n_output
+        self._activation = activation
 
-    def _activation_function(self, activation, product):
-        if activation == "softmax":
-            output = np.exp(product)/np.sum(np.exp(product))
-            return output
+    def _activate(self, product):
+        if self._activation == "softmax":
+            z = self._sigmoid_activation(product)
+        return z
 
 
 class Dropout(Layer):
